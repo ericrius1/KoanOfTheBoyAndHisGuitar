09cb725d33a7d0608f7f8079415da3db1fdeef3c
Eric <Ericrius1@gmail.com>
2014-07-09 15:20:00 +0200

    wind

--- js/grass.js
+++ js/grass.js
@@ -1,23 +1,25 @@
 var Grass = function(groundMesh) {
-  var drawArray = [];
   var lineGeo = new THREE.Geometry();
 
-  var attributes = {
-    draw: {type: 'f', value: []}
+  var grassAttributes = {
+    draw: {type: 'f', value: []},
+    windFactor: {type: 'f', value: []}
   }
 
-  var shaderMaterial = new THREE.ShaderMaterial({
+  var grassMaterial = new THREE.ShaderMaterial({
     attributes: attributes,
     vertexShader: shaders.vertexShaders.lines,
     fragmentShader: shaders.fragmentShaders.lines
   });
   createBlades();
 
-  var lines = new THREE.Line(lineGeo, shaderMaterial, THREE.LineStrip);
+  var lines = new THREE.Line(lineGeo, grassMaterial, THREE.LineStrip);
   scene.add(lines);
 
 
   function createBlades() {
+    var drawArray = [];
+    var windArray = [];
     var points = THREE.GeometryUtils.randomPointsInGeometry(groundMesh.geometry, 100);
     for (var i = 0; i < points.length; i++) {
       var bladeHeight = _.random(4, 6);
@@ -25,6 +27,8 @@ var Grass = function(groundMesh) {
       for (var j = 0; j < bladeHeight; j++) {
         var vertex = new THREE.Vector3(point.x, j/2, point.y);
         lineGeo.vertices.push(vertex);
+        var nH = vertex.y/bladeHeight + 0.5
+        windArray.push(nh * nH * nH);
         if( j ===0 || j === bladeHeight -1){
           drawArray.push(0)
         }
@@ -35,9 +39,12 @@ var Grass = function(groundMesh) {
     }
 
     var vertices = lineGeo.vertices;
-    var values_draw = attributes.draw.value;
+    var values_draw = grassAttributes.draw.value;
+    var values_wind = grassAttributes.windFactor.value;
+    //copy array values into attribute array
     for(var v = 0; v < vertices.length; v++){
-      values_draw [ v ] = drawArray[v]; 
+      values_draw [ v ] = drawArray[ v ];
+      values_ wind [ v ] = windArray[ v ];
     }
   }
   this.update = function() {
